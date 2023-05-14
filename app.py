fa749be9d86c4f3ef6512d00b443bd628f2b54fe
udenis80 <udenis80@mail.ru>
2023-05-14 16:44:53 +0300

    first app in flask

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for,request
+from flask import Flask, render_template, url_for, request, flash, redirect
+from pip._internal.network import session
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'ksflaghk2jlfg4hfd43gjkh'
 menu = [{ 'name': 'Установка', 'url': 'install-flask'},
         { 'name': 'Первое приложение', 'url': 'first-app'},
         { 'name': 'Обратная связь', 'url': 'contact'}]
@@ -23,8 +24,25 @@ def about():
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено')
+        else:
+            flash('Ошибка отправки')
+        print(request.form['username'])
     return render_template('contact.html', title='Обратная свзь', menu=menu)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'user_logged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.form['username'] == 'abc' and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', tittle='Авторизация', menu=menu)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title='Страница не найдена', menu=menu), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
