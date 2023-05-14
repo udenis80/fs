3ceee8c2c59470175aea469cac7645fffd67b86e
udenis80 <udenis80@mail.ru>
2023-05-14 13:59:15 +0300

    first app in flask

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 menu = ['Установка', ' Первое приложение', 'Обратная связь']
 @app.route('/')
-def index():  #стартовая страница
-    return render_template('index.html', title='про фласк', menu=menu)
+def index():
+    print(url_for('index'))#стартовая страница
+    return render_template('index.html', menu=menu)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='О нас')
+    print(url_for('about'))
+    return render_template('about.html', title='О сайте', menu=menu)
 
 if __name__ == '__main__':
     app.run(debug=True)
