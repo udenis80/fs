d09c5f5fb0979a2837f643019097e7c3853ec220
udenis80 <udenis80@mail.ru>
2023-05-14 15:22:41 +0300

    first app in flask

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request
 
 app = Flask(__name__)
 
-menu = ['Установка', ' Первое приложение', 'Обратная связь']
+menu = [{ 'name': 'Установка', 'url': 'install-flask'},
+        { 'name': 'Первое приложение', 'url': 'first-app'},
+        { 'name': 'Обратная связь', 'url': 'contact'}]
 @app.route('/')
 def index():
-    print(url_for('index'))#стартовая страница
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route('/profile/<path:username>')
@@ -18,8 +20,16 @@ def about():
     print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
 
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('contact.html', title='Обратная свзь', menu=menu)
+
 if __name__ == '__main__':
     app.run(debug=True)
-
+#
 # with app.test_request_context():
 #     print(url_for('about'))
+#     print(url_for('index'))
+#     print(url_for('profile', username='abc'))
