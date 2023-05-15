795b9712814e8f40de467fe623416db5145190b8
udenis80 <udenis80@mail.ru>
2023-05-15 23:57:42 +0300

    first app in flask

--- FDataBase.py
+++ FDataBase.py
@@ -1,3 +1,8 @@
+import math
+import sqlite3
+import time
+
+
 class FDataBase:
     def __init__(self, db):
         self.__db = db
@@ -12,3 +17,13 @@ class FDataBase:
         except:
             print('Ошибка чтения из БД')
         return []
+
+    def addPost(self, title, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('Ошибка добавления статьи в БД' + str(e))
+            return False
+        return True
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import g
 import os
 import sqlite3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 from FDataBase import FDataBase
 
@@ -34,17 +34,36 @@ def get_db():
         g.link_db = connect_db()
     return g.link_db
 
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с базой данных, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 @app.route('/')
 def index():
     db = get_db()
     dbase = FDataBase(db)
     return render_template('index.html', menu=dbase.getMenu())
 
-@app.teardown_appcontext
-def close_db(error):
-    '''Закрываем соединение с базой данных, если оно было установлено'''
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
+@app.route('/add_post', methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title='Добавление статьи')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
