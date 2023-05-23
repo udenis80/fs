ed327dfc0f229165e5bb1027563f6ddfe7e397f0
udenis80 <udenis80@mail.ru>
2023-05-24 01:00:02 +0300

    use Blueprint

--- admin/admin.py
+++ admin/admin.py
@@ -1,6 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, flash, redirect, url_for, request, session
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+def login_admin():
+    session['admin_logged'] = 1
+
+def islogged():
+    return True if session.get('admin_logged') else False
+
+def logout_admin():
+    session.pop('admin_logged', None)
+
 @admin.route('/')
 def index():
     return 'admin'
+
+@admin.route('/login', method=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == '12345':
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash('Неверная пара логин/пароль', "error")
+    return render_template('admin/login.html', title='Админ-панель')
