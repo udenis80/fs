8930085609e595dc6c5601d725b7728d0ff06156
udenis80 <udenis80@mail.ru>
2023-05-25 01:39:07 +0300

    use Blueprint

--- admin/admin.py
+++ admin/admin.py
@@ -5,18 +5,26 @@ admin = Blueprint('admin', __name__, template_folder='templates', static_folder=
 def login_admin():
     session['admin_logged'] = 1
 
-def islogged():
+def isLogged():
     return True if session.get('admin_logged') else False
 
 def logout_admin():
     session.pop('admin_logged', None)
 
+menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
 @admin.route('/')
 def index():
-    return 'admin'
+    if not isLogged():
+        return redirect(url_for('.login'))
+    return render_template('admin/index.html', menu=menu, title='Админ-панель')
 
-@admin.route('/login', method=['POST', 'GET'])
+@admin.route('/login', methods=['POST', 'GET'])
 def login():
+    if isLogged():
+        return redirect(url_for('.index'))
+
     if request.method == "POST":
         if request.form['user'] == "admin" and request.form['psw'] == '12345':
             login_admin()
@@ -27,7 +35,7 @@ def login():
 
 @admin.route('/logout', methods=['POST', 'GET'])
 def logout():
-    if not islogged():
+    if not isLogged():
         return redirect(url_for('.login'))
     logout_admin()
     return redirect(url_for('.login'))
